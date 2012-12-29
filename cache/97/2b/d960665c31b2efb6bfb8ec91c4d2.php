deb6e019eb524196ca97de727fc8b8cd0b30904a
Vitaliy Serov <serovvitaly@gmail.com>
2012-12-29 09:24:45 +0400

    установлен Twig

---
+++ cache/97/2b/d960665c31b2efb6bfb8ec91c4d2.php
@@ -0,0 +1,38 @@
+<?php
+
+/* index.html */
+class __TwigTemplate_972bd960665c31b2efb6bfb8ec91c4d2 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Привет 800 ";
+        if (isset($context["name"])) { $_name_ = $context["name"]; } else { $_name_ = null; }
+        echo twig_escape_filter($this->env, $_name_, "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
