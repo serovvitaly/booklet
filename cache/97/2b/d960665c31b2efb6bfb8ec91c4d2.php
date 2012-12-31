3f3fbfd3d8c570b03f4c0c5fb9040ea49e7c66d9
Vitaly Serov <serovvitaly@gmail.com>
2012-12-31 09:59:37 +0600

    сделан слайдер

--- cache/97/2b/d960665c31b2efb6bfb8ec91c4d2.php
+++ cache/97/2b/d960665c31b2efb6bfb8ec91c4d2.php
@@ -16,9 +16,42 @@ class __TwigTemplate_972bd960665c31b2efb6bfb8ec91c4d2 extends Twig_Template
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 1
-        echo "Привет 800 ";
-        if (isset($context["name"])) { $_name_ = $context["name"]; } else { $_name_ = null; }
-        echo twig_escape_filter($this->env, $_name_, "html", null, true);
+        echo "<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+  <title>Untitled</title>
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"/style/css/reset.min.css\">
+  
+  
+  <script type=\"text/javascript\" src=\"/style/js/jquery-1.8.3.min.js\"></script>
+  
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"/woothemes/flexslider.css\">
+  <script type=\"text/javascript\" src=\"/woothemes/jquery.flexslider.js\"></script>
+  
+  <script type=\"text/javascript\">
+    \$(document).ready(function() {
+      \$('.flexslider').flexslider({
+        animation: 'slide',
+        itemWidth: 607,
+        itemHeight: 500,
+      });
+    })
+  </script>
+  
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"/style/css/styles.css\">
+</head>
+<body>
+  <div style=\"height: 500px; background: red;\">
+  
+  ";
+        // line 29
+        $this->env->loadTemplate("base/slider.html")->display($context);
+        // line 30
+        echo "  
+  </div>
+</body>
+</html>";
     }
 
     public function getTemplateName()
@@ -33,6 +66,6 @@ class __TwigTemplate_972bd960665c31b2efb6bfb8ec91c4d2 extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  19 => 1,);
+        return array (  51 => 30,  49 => 29,  19 => 1,);
     }
 }
