3c021bdc6b5b4096b9658682b55cc06a9370f4f5
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-09 23:49:26 +0400

    сделана кнопка

--- cache/97/2b/d960665c31b2efb6bfb8ec91c4d2.php
+++ cache/97/2b/d960665c31b2efb6bfb8ec91c4d2.php
@@ -29,6 +29,8 @@ class __TwigTemplate_972bd960665c31b2efb6bfb8ec91c4d2 extends Twig_Template
   <link rel=\"stylesheet\" type=\"text/css\" href=\"/woothemes/flexslider.css\">
   <script type=\"text/javascript\" src=\"/woothemes/jquery.flexslider.js\"></script>
   
+  <script type=\"text/javascript\" src=\"http://www.bitstorm.org/jquery/color-animation/jquery.animate-colors-min.js\"></script>
+  
   <script type=\"text/javascript\">
     \$(document).ready(function() {
       \$('.flexslider').flexslider({
@@ -36,19 +38,46 @@ class __TwigTemplate_972bd960665c31b2efb6bfb8ec91c4d2 extends Twig_Template
         itemWidth: 607,
         itemHeight: 500,
       });
+      
+      \$('.body').animate({
+          backgroundColor: '#0000FF'
+      }, 10000);
+      
+      \$('.dis-anchor').mouseover(function(){
+          \$(this).animate({
+              opacity: 1
+          }, 500);
+      }).mouseout(function(){
+          \$(this).animate({
+              opacity: 0
+          }, 500);
+      });
+      
     })
   </script>
+  
+  <style type=\"text/css\">
+    .dis-anchor{
+        background: url(/images/like-me.png) no-repeat center;
+        opacity: 0;
+        height: 420px;
+        width: 527px;
+        top: 0;
+        position: absolute;
+        display: block;
+        margin: 40px 0 0 40px;
+    }
+  </style>
   
   <link rel=\"stylesheet\" type=\"text/css\" href=\"/style/css/styles.css\">
 </head>
 <body>
-  <div style=\"height: 500px; background: red;\">
-  
+  <div class=\"body\" style=\"height: 500px; background: red;\">
   ";
-        // line 29
+        // line 58
         $this->env->loadTemplate("base/slider.html")->display($context);
-        // line 30
-        echo "  
+        // line 59
+        echo "    <a class=\"dis-anchor\" href=\"#\"></a>
   </div>
 </body>
 </html>";
@@ -66,6 +95,6 @@ class __TwigTemplate_972bd960665c31b2efb6bfb8ec91c4d2 extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  51 => 30,  49 => 29,  19 => 1,);
+        return array (  80 => 59,  78 => 58,  19 => 1,);
     }
 }
