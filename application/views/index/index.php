03e003a8020ed064c21f80091053dc22be159c70
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-16 00:08:31 +0400

    добавлена нижняя пагинация

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -41,3 +41,15 @@ if (isset($products) AND count($products) > 0) {
 }
 ?>
 </div>
+
+<div class="pagination pagination-centered pagination-fixed-bottom">
+  <ul>
+    <li class="disabled"><a href="#">«</a></li>
+    <li class="active"><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li><a href="#">»</a></li>
+  </ul>
+</div>
