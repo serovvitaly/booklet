228ff111d8c9b9bd77bb455a61686352a2906b52
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-15 18:40:01 +0400

    фикс вывода карзины покупателя

--- application/classes/Controller/Base.php
+++ application/classes/Controller/Base.php
@@ -17,6 +17,30 @@ class Controller_Base extends Controller_Template {
             $this->template->content = 'Content is empty';
         }
         
+        
+        $basket_empty   = true;
+        $basket_content = 'Корзина пуста';
+        
+        if (USER_ID > 0) {
+            
+            $user = ORM::factory('User', USER_ID);
+            
+            $user_basket = $user->get_basket();
+            
+            if ($user_basket['total_count'] > 0) {
+                
+                $basket_empty   = false;
+                
+                $basket_content = "товаров - {$user_basket['total_count']}, на {$user_basket['summa']} руб.";
+            }
+        }
+        
+        $this->template->basket = array(
+            'empty'   => $basket_empty,
+            'content' => $basket_content
+        );
+        
+        
         parent::after();
     }
 
--- application/classes/Controller/Index.php
+++ application/classes/Controller/Index.php
@@ -8,29 +8,6 @@ class Controller_Index extends Controller_Base {
         
 		$this->template->content->products = ORM::factory('Product')->limit(42)->find_all();
         
-        
-        $basket_empty   = true;
-        $basket_content = 'Корзина пуста';
-        
-        if (USER_ID > 0) {
-            
-            $user = ORM::factory('User', USER_ID);
-            
-            $user_basket = $user->get_basket();
-            
-            if ($user_basket['total_count'] > 0) {
-                
-                $basket_empty   = false;
-                
-                $basket_content = "товаров - {$user_basket['total_count']}, на {$user_basket['summa']} руб.";
-            }
-        }
-        
-        $this->template->basket = array(
-            'empty'   => $basket_empty,
-            'content' => $basket_content
-        );
-        
 	}
 
 } // End
