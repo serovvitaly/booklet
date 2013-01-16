732d2a3603ab566285503e3a5fe903dd0873f931
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-16 23:14:15 +0400

    мелкий рестайлинг главной страницы

--- application/views/_layout/default.php
+++ application/views/_layout/default.php
@@ -11,23 +11,14 @@
   <script type="text/javascript" src="/vendor/bootstrap/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="/style/default/js/functions.js"></script>
   
-  <link rel="stylesheet" href="/vendor/flexpager/flexpager.css">
-  <script src="/vendor/flexpager/jquery.flexpager.js"></script>
   
   <script type="text/javascript">
     $(document).ready(function(){
         
         setBody();
+        $(window).resize(function(){ setBody(); });
         
-        $(window).resize(function(){
-            setBody();
-        });
-        
-        $('body').flexpager({
-            startContent: {
-                url: 'nolayout'
-            }
-        });     
+             
     });
     
     function setBody(){
@@ -37,7 +28,7 @@
   </script>
   
 </head>
-<body style="margin: 0; padding: 0;">
-  
+<body>
+  <?= $content ?>
 </body>
 </html>
