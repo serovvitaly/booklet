e3ad45e643b7c2f6ddcb02ea21b76f100c7fcd4d
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-16 15:58:54 +0400

    рестайлинг верхнего меню

--- application/views/_common/topmenu.php
+++ application/views/_common/topmenu.php
@@ -1,8 +1,11 @@
 <!-- <a class="logo" href="/" title="На стартовую"><img src="/style/default/img/logo.png"></a> -->
 
-<div>
-  <a href="/help/delivery/">Оплата и доставка</a>
+<div class="topsubmenu">
   <a href="/help/rules/">Правила</a>
+  <span>&bull;</span>
+  <a href="/help/delivery/">Оплата и доставка</a>
+  <span>&bull;</span>
+  <a href="/help/rules/">Обратная связь</a>
 </div>
 
 <div class="navbar topmenu" style="margin: 0; width: 700px;">
@@ -28,6 +31,16 @@
       <li class="divider-vertical"></li>     
     </ul>
     <ul class="nav pull-right">
+      <li><a href="#">«</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span id="pagination-top-page">1</span> <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="/help/delivery/">2</a></li>
+          <li><a href="/help/rules/">3</a></li>
+          <li><a href="/help/rules/">4</a></li>
+        </ul>
+      </li>
+      <li><a href="#">»</a></li>
       <li class="dropdown active">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i id="basket-icon" class="icon-shopping-cart" style="margin: 0 10px 0 0"></i> <span id="basket-content">Корзина пуста</span> <b id="basket-menu" style="display:none" class="caret"></b></a>
         <ul class="dropdown-menu">
