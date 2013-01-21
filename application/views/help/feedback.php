0d3857f500b303ac39976b6f6926ca87573e0ba3
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-21 08:23:55 +0400

    начата страница "Обратная связь"

--- application/views/help/feedback.php
+++ application/views/help/feedback.php
@@ -1,1 +1,18 @@
-feedback
+<div style="padding: 16px 20px 20px;">
+
+  <form class="form-horizontal">
+    <div class="control-group">
+      <label class="control-label" for="inputFbTitle">Тема</label>
+      <div class="controls">
+        <input style="width: 66%;" type="text" id="inputFbTitle">
+      </div>
+    </div>
+    <div class="control-group">
+      <label class="control-label" for="inputFbMessage">Сообщение</label>
+      <div class="controls controls-row">
+        <textarea style="width: 66%;" rows="7" cols="" id="inputFbMessage"></textarea>
+      </div>
+    </div>
+  </form>
+
+</div>
